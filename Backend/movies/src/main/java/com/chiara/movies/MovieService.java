666eecda83b5505d45733f7570f67139f7bb70af
Chiara Pigoni <m.chiarapigoni@gmail.com>
2024-04-19 17:13:27 +0100

    mapping to single id movie added

--- Backend/movies/src/main/java/com/chiara/movies/MovieService.java
+++ Backend/movies/src/main/java/com/chiara/movies/MovieService.java
@@ -1,9 +1,11 @@
 package com.chiara.movies;
 
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class MovieService {
@@ -13,4 +15,8 @@ private  MovieRepository movieRepository;
     public List<Movie> allMovies() {
         return movieRepository.findAll();
     }
+    public Optional<Movie> singleMovie(ObjectId id) {
+    return movieRepository.findById(id);
+    }
+
 }
