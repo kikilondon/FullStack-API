8db72a7debfaabca90e71c8bfb00e31bf627ab19
Chiara Pigoni <m.chiarapigoni@gmail.com>
2024-04-27 18:08:16 +0100

    add create and push new review into MongoDb repository

--- Backend/movies/src/main/java/com/chiara/movies/ReviewService.java
+++ Backend/movies/src/main/java/com/chiara/movies/ReviewService.java
@@ -1,14 +1,33 @@
 package com.chiara.movies;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Update;
 import org.springframework.stereotype.Service;
 
+import static org.springframework.data.mongodb.core.aggregation.SelectionOperators.First.first;
+
+// create a review to push into a specific movie section into MongoDB
 @Service
 public class ReviewService {
+    @Autowired
+
+    private ReviewRepository reviewRepository;
+@Autowired
+    private MongoTemplate mongoTemplate;
+//we receive from the user the reviewBody and the imdbId to upload a review
     public Review createReviewBy(String reviewBody, String imdbId){
+//new object
+        Review review = reviewRepository.insert(new Review(reviewBody));
 
-        Review review = new Review(reviewBody);
+        reviewRepository.insert(review);
+        mongoTemplate.update(Movie.class)
+                .matching(Criteria.where("imdbId").is(imdbId))
+                //push new review inside the reviewIds of a specific movie into MongoDB
+                .apply(new Update().push( "reviewIds").value(review))
+                .first();// push as first into the repository
         return review;
-
 
     }
 
