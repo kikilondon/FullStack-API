6582098ab48d09683f280a11f679f51a3455b1cf
Chiara Pigoni <m.chiarapigoni@gmail.com>
2024-05-02 21:01:40 +0100

    create logic for hero component

--- Frontend/movie-api/src/App.js
+++ Frontend/movie-api/src/App.js
@@ -31,7 +31,7 @@ function App() {
   return (
     <div className="App">
        <Routes>
-        <Route  path="/" element= {Layout}>
+        <Route  path="/" element= {<Layout/>}>
            <Route path="/" element={<Home/>} />
 
 
