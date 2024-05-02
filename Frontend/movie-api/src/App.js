9bc23d8cc4e4a5eda0880134b5e35b26f1f9c21f
Chiara Pigoni <m.chiarapigoni@gmail.com>
2024-05-02 20:23:34 +0100

    add Routes/Route in App

--- Frontend/movie-api/src/App.js
+++ Frontend/movie-api/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import api from './api/axiosConfig';
 import React, { useState, useEffect } from'react';
 import Layout from './components/Layout';
+import {Routes, Route} from "react-router-dom"
 
 function App() {
 
@@ -28,7 +29,13 @@ function App() {
 
   return (
     <div className="App">
-     
+       <Routes>
+        <Route  path="/" element= {Layout}>
+          
+
+
+        </Route>
+       </Routes>
     </div>
   );
 }
